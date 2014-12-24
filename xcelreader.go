c64f09e7d14c08d8cbcb17301004434aad15a2ef
pamidu <pamidu@live.com>
2014-12-24 14:03:45 +0530

    2014-12-23

--- xcelreader.go
+++ xcelreader.go
@@ -6,17 +6,53 @@ import (
 )
 
 func main() {
+
+	//initliz
 	excelFileName := "excel.xlsx"
+	rowcount := 0
+	colunmcount := 0
+	var exceldata []map[string]string
+	var colunName []string
+
+	//file read
 	xlFile, error := xlsx.OpenFile(excelFileName)
+
 	if error == nil {
 		for _, sheet := range xlFile.Sheets {
+			//max colunm count and row count
+			rowcount = sheet.MaxRow
+			colunmcount = sheet.MaxCol
+			//make colunm titile  to arry
+			colunName = make([]string, colunmcount)
 			for _, row := range sheet.Rows {
-				for _, cell := range row.Cells {
-					fmt.Printf("%s ", cell.String())
+				for j, cel := range row.Cells {
+					colunName[j] = cel.String()
 				}
-				fmt.Printf("\n")
+				break
 			}
 		}
 	}
+	fmt.Println(rowcount, "ghjkl")
+	exceldata = make(([]map[string]string), rowcount)
+
+	if error == nil {
+		for _, sheet := range xlFile.Sheets {
+			fmt.Println(sheet.MaxCol, sheet.MaxRow)
+			for rownumber, row := range sheet.Rows {
+				currentRow := make(map[string]string)
+				exceldata[rownumber] = currentRow
+				for cellnumber, cell := range row.Cells {
+					fmt.Println(rownumber + 1)
+					fmt.Println(colunName[cellnumber])
+					fmt.Println(cell.String())
+
+					exceldata[rownumber][colunName[cellnumber]] = cell.String()
+				}
+				fmt.Printf("\n")
+
+			}
+		}
+	}
+	fmt.Println(rowcount, colunmcount, colunName, exceldata)
 
 }
